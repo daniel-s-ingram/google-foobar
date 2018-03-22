d1313e9efb7c4c56f6827b8f0856ed8323bca161
Daniel Ingram <ingramds@appstate.edu>
2018-03-22 09:39:10 -0400

    Update the_grandest_staircase_of_them_all.py

--- Level3/the_grandest_staircase_of_them_all/the_grandest_staircase_of_them_all.py
+++ Level3/the_grandest_staircase_of_them_all/the_grandest_staircase_of_them_all.py
@@ -1,17 +1,21 @@
 #The number of different staircases that can be formed out of n bricks
 #is equal to the number of distinct integer partitions of n
-def count_distinct_partitions(n):
-	table = [[0 for _ in xrange(n+1)] for _ in xrange(n+1)]
+def count_distinct_partitions(m):
+	table = [[0 for _ in xrange(m+1)] for _ in xrange(m+1)]
 	table[0][0] = 1
 
-	#Need to do more research before I can comment on why this works
-	for i in xrange(1, n+1):
-		for j in xrange(n+1):
-			table[i][j] = table[i-1][j]
-			if j >= i:
-				table[i][j] = table[i][j]+table[i-1][j-i]
+	'''
+	The number of partitions of a number n into at least k parts equals the number of partitions into exactly k parts
+	plus the number of partitions into at least k-1 parts. Subtracting 1 from each part of a partition of n into k parts
+	gives a partition of n-k into k parts. These two facts together are used for this algorithm.
+	'''
+	for n in xrange(1, m+1):
+		for k in xrange(m+1):
+			table[n][k] = table[n][k-1]
+			if n >= k:
+				table[n][k] += table[n-k][k]
 
-	return table[n][n]-1
+	return table[m][m]
 
 def answer(n):
     # your code here
